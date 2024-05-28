7a1c9a3667bfa6ca7b09297094debce4202dd8fb
Prinay <prinaypanday@gmail.com>
2024-05-29 00:31:05 +0200

    Add a test solution showing the use of architecture tests.

---
+++ ArchitectureTestsBlog/Data/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccess.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public List<string> GetUserNames()
+        {
+            return new List<string>
+            {
+                "John",
+                "Doe"
+            };
+        }
+    }
+}
